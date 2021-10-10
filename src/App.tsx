db83724121a2e23e5d71afc632c74c19405ac55e
gomjellie <gomjellie@gmail.com>
2021-10-11 07:43:18 +0900

    Styled Component props 타입 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,21 @@ import styled, {ThemeProvider} from 'styled-components/native';
 import {DarkTheme, LightTheme} from './Theme';
 
 interface IContainerProps {
-  theme?: ITheme;
+  theme: ITheme;
 }
 
-const Container = styled.View`
+const Container = styled.View<IContainerProps>`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: ${(props: IContainerProps) =>
-    props.theme && props.theme.color.background};
+  background-color: ${props => props.theme.color.background};
 `;
 
-const MainText = styled.Text`
+const MainText = styled.Text<{theme: ITheme}>`
   font-size: 20px;
   text-align: center;
   margin: 10px;
-  color: red;
+  color: ${props => props.theme.color.text};
 `;
 
 const App: FC = () => {
